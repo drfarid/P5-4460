aaee7cbf6d858564944d0c5957c5bf2349286d28
drfarid <habichuelas21@gmail.com>
2019-04-13 16:48:06 -0400

    started setting up the map

--- main.js
+++ main.js
@@ -1,90 +1,96 @@
+var counterUS = 0;
+var clickedCandy = null;
 
+
+google.charts.load('current', {
+	    'packages':['geochart'],
+	    'mapsApiKey': 'AIzaSyAgyOWtjPkSfyInVxvcdUY6wVGXIM7evDU'
+});
+
+google.setOnLoadCallback(function () {
+    createMap(clickedCandy);
+});
+
+
+
+//main csv function that creates scatterplot and heatmap
 d3.csv("candy.csv", function(csv) {
-    
+    var actualReviews = csv.filter(function(d){
 
-
-
-
-	//============================Thomas==================================
-    //reformat the locations to have a consistent format
-    //every entry in q5 is changed to an abbreviation of 2 letters
-    //the abbreviation lists are in the folder named such
-    var updatedAbbr = csv.map(function(d) {
-
-    	//check to see the entry has a value
-	    if (d.Q5_STATE != null) { 
-	  		
-	  		//the country is the united States
-	    	if (d.Q4_COUNTRY == "United States") {
-	    		//if the state value is longer than two characters, find the abbreviation
-	    		if (d.Q5_STATE.length > 2) {
-	    			var usAbbreviation = findAbbreviation("USA", d.Q5_STATE);
-	    			setTimeout(function() {
-						usAbbreviation = returnValue;
-						if (usAbbreviation != null) {
-		    				d.Q5_STATE = returnValue;	
-		    			}
-					},2000);	
-	    		}
-
-	    	//the country is canada
-	    	} else if (d.Q4_COUNTRY == "Canada") {
-	    		//if the state value is longer than two characters, find the abbreviation
-	    		if (d.Q5_STATE.length > 2) {
-	    			var usAbbreviation = findAbbreviation("USA", d.Q5_STATE);
-	    			setTimeout(function() {
-						usAbbreviation = returnValue;
-						if (usAbbreviation != null) {
-		    				d.Q5_STATE = returnValue;	
-		    			}
-					},2000);	
-	    		}
-	    	//the country is not the United States or Canada
-	    	//do not use this entry
-	    	}
-	    }
     });
 
 
-    //============================Thomas==================================
 });
 
-var returnValue = null;
-//find the abbreviation of the passed in state, if country == usa, or province if country == canada
-function findAbbreviation(country, fullName) {
+
+function createMap(candyName) {
+
+    var data = google.visualization.arrayToDataTable([
+      ['State', 'Popularity'],
+      ['AL', getAverageScore(candyName, 'AL')],
+      ['AK', getAverageScore(candyName, 'AK')],
+      ['AZ', getAverageScore(candyName, 'AZ')],
+      ['AR', getAverageScore(candyName, 'AR')],
+      ['CA', getAverageScore(candyName, 'CA')],
+      ['CO', getAverageScore(candyName, 'CO')],
+      ['CT', getAverageScore(candyName, 'CT')],
+      ['DE', getAverageScore(candyName, 'DE')],
+      ['FL', getAverageScore(candyName, 'FL')],
+      ['GA', getAverageScore(candyName, 'GA')],
+      ['HI', getAverageScore(candyName, 'HI')],
+      ['ID', getAverageScore(candyName, 'ID')],
+      ['IL', getAverageScore(candyName, 'IL')],
+      ['IN', getAverageScore(candyName, 'IN')],
+      ['IA', getAverageScore(candyName, 'IA')],
+      ['KS', getAverageScore(candyName, 'KS')],
+	  ['KY', getAverageScore(candyName, 'KY')],
+	  ['LA', getAverageScore(candyName, 'LA')],
+	  ['ME', getAverageScore(candyName, 'ME')],
+	  ['MD', getAverageScore(candyName, 'MD')],
+	  ['MA', getAverageScore(candyName, 'MA')],
+	  ['MI', getAverageScore(candyName, 'MI')],
+	  ['MN', getAverageScore(candyName, 'MN')],
+	  ['MS', getAverageScore(candyName, 'MS')],
+	  ['MO', getAverageScore(candyName, 'MO')],
+	  ['MT', getAverageScore(candyName, 'MT')],
+	  ['NE', getAverageScore(candyName, 'NE')],
+	  ['NV', getAverageScore(candyName, 'NV')],
+	  ['NH', getAverageScore(candyName, 'NH')],
+	  ['NJ', getAverageScore(candyName, 'NJ')],
+	  ['NM', getAverageScore(candyName, 'NM')],
+	  ['NY', getAverageScore(candyName, 'NY')],
+	  ['NC', getAverageScore(candyName, 'NC')],
+	  ['ND', getAverageScore(candyName, 'ND')],
+	  ['OH', getAverageScore(candyName, 'OH')],
+	  ['OK', getAverageScore(candyName, 'OK')],
+	  ['OR', getAverageScore(candyName, 'OR')],
+	  ['PA', getAverageScore(candyName, 'PA')],
+	  ['RI', getAverageScore(candyName, 'RI')],
+	  ['SC', getAverageScore(candyName, 'SC')],
+	  ['SD', getAverageScore(candyName, 'SD')],
+	  ['TN', getAverageScore(candyName, 'TN')],
+	  ['TX', getAverageScore(candyName, 'TX')],
+	  ['UT', getAverageScore(candyName, 'UT')],
+	  ['VT', getAverageScore(candyName, 'VT')],
+	  ['VA', getAverageScore(candyName, 'VA')],
+	  ['WA', getAverageScore(candyName, 'WA')],
+	  ['WV', getAverageScore(candyName, 'WV')],
+	  ['WI', getAverageScore(candyName, 'WI')],
+	  ['WY', getAverageScore(candyName, 'WY')]
+    ]);
+
+    var options = {
+      region: 'US',
+      displayMode: 'regions',
+      resolution: 'provinces',
+    };
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(data, options);
+}
+
+function getAverageScore(candyName, abbreviation) {
+	var avgScore = d3.
 	
-
-	if (country == "USA") {
-		d3.csv("usStatesAndAbbreviations.csv", function(csv) {
-			getUsAbbreviation(fullName, csv);
-		});
-	} else {
-		d3.csv("canadaProvincesAndAbbreviations.csv", function(csv) {
-			getCanAbbreviation(fullName, csv);
-		});
-	}
-
-	setTimeout(function(){
-		return returnValue;
-	},200);
-}
-
-function getUsAbbreviation(fullName, csv) {
-	var ret = csv.filter(function(d) {
-		if (fullName == d.full) {
-			// console.log("joy and peace: " + d.abbreviation);
-			returnValue = d.abbreviation;
-			return;
-		}
-	});
-}
-
-function getCanAbbreviation(fullName, csv) {
-	var ret = csv.filter(function(d) {
-		if (fullName == d.fullname) {
-			// console.log("joy and peace: " + d.abbreviation);
-			returnValue = d.abbreviation;
-			return;
-		}
-	});
 }
